a7efe9893177ec226b88b6bf069004101b24ca33
Anik Maharjan <maharjanprincetag@gmail.com>
2025-02-09 10:45:17 +0545

    done

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import NavBar from './NavBar'
 import {FaGithub,FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
